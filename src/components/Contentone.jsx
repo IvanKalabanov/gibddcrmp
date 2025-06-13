9bc9c97fd7b023f0f6a20ede5823a750c22d316e
Иван <kalabanovivan3@gmail.com>
2025-06-14 00:07:10 +0300

    Initial commit: React project setup

--- src/components/Contentone.jsx
+++ src/components/Contentone.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Contentone.css"
+import image281 from "./img/281.png";
+import image2186 from "./img/2186_logo.jpg";
 
 function Contentone () {
     return(
@@ -11,49 +13,49 @@ function Contentone () {
                     <h2>Начальник УГИБДД по Нижегородской области</h2>
                     <h2>Гуров Иван Алексеевич </h2> 
                     <h2>Полковник полиции</h2>
-                    <img src="src/components/img/281.png"></img>
+                    <img src={image281}></img>
                 </div>
                 <div className="square">
                     <h2>Заместитель начальника УГИБДД по Нижегородской области</h2>
                     <h2>ВАКАНТНО</h2> 
                     <h2>ВАКАНТНО</h2>
-                    <img src="src/components/img/2186_logo.jpg"></img>
+                    <img src={image2186}></img>
                 </div>
                 <div className="square">
                     <h2>Командир отдельного батальона</h2>
                     <h2>ВАКАНТНО</h2> 
                     <h2>ВАКАНТНО</h2>
-                    <img src="src/components/img/2186_logo.jpg"></img>
+                    <img src={image2186}></img>
                 </div>
                 <div className="square">
                     <h2>Заместитель командира отдельного батальона</h2>
                     <h2>ВАКАНТНО</h2> 
                     <h2>ВАКАНТНО</h2>
-                    <img src="src/components/img/2186_logo.jpg"></img>
+                    <img src={image2186}></img>
                 </div>
                 <div className="square">
                     <h2>Командир отдельной роты</h2>
                     <h2>ВАКАНТНО</h2> 
                     <h2>ВАКАНТНО</h2>
-                    <img src="src/components/img/2186_logo.jpg"></img>
+                    <img src={image2186}></img>
                 </div>
                 <div className="square">
                     <h2>Заместитель командира отдельной роты</h2>
                     <h2>ВАКАНТНО</h2> 
                     <h2>ВАКАНТНО</h2>
-                    <img src="src/components/img/2186_logo.jpg"></img>
+                    <img src={image2186}></img>
                 </div>
                 <div className="square">
                     <h2>Командир специализированной роты</h2>
                     <h2>ВАКАНТНО</h2> 
                     <h2>ВАКАНТНО</h2>
-                    <img src="src/components/img/2186_logo.jpg"></img>
+                    <img src={image2186}></img>
                 </div>
                 <div className="square">
                     <h2>Заместитель командира специализированной роты</h2>
                     <h2>ВАКАНТНО</h2> 
                     <h2>ВАКАНТНО</h2>
-                    <img src="src/components/img/2186_logo.jpg"></img>
+                    <img src={image2186}></img>
                 </div>
             </div>
         </div>
