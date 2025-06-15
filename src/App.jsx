a2e6d8f9ca5d219e94f4f37390aead7e46c46ecf
IvanKalabanov <kalabanovivan3@gmail.com>
2025-06-15 23:23:17 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Contentone from './components/Contentone';
 import Base from './components/Base';
 import VehiclePark from './components/VehiclePark';
 import Footer from './components/Footer';
+import GibddNews from './components/GibddNews';
+import DetailedNews from './components/DetailedNews';
 
-function App() {
+// Компонент-обёртка для существующего layout
+function MainLayout() {
   return (
     <div className="app">
       <Header />
+      <Outlet /> {/* Сюда подставляются маршруты */}
+      <Footer />
+    </div>
+  );
+}
+
+// Компонент для главной страницы (со всеми секциями)
+function HomePage() {
+  return (
+    <>
+      <GibddNews />
       <Contentone />
       <Base />
       <VehiclePark />
-      <Footer />
-    </div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Основной layout с Header/Footer */}
+        <Route path="/" element={<MainLayout />}>
+          {/* Главная страница со всеми компонентами */}
+          <Route index element={<HomePage />} />
+          
+          {/* Страница детальной новости */}
+          <Route path="/news/:id" element={<DetailedNews />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
